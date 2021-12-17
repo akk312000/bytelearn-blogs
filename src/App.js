8e437a2826c63798e6aba4979528eaece716f100
Ashish Kashyap <akk312000@gmail.com>
2021-12-17 10:17:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home/>}>
+            </Route>
+            <Route path="/create" element={<Create/>}>
+            </Route>
+            <Route path="/blogs/:id" element={<BlogDetails/>}>
+            </Route>
+            <Route path="*" element={<NotFound/>}>
+             
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>React Blog App</h1>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/create">Create Blog</Link>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
